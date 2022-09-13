f3607994916e74d158d73a8d231762c117116594
MusaAkyuz <musakyuz0@gmail.com>
2022-09-13 15:45:17 +0300

    main

---
+++ Python-For-Everyone-Michigan-University/inputs.py
@@ -0,0 +1,9 @@
+'''
+Write a program that uses input to prompt a user for their name and
+then welcomes them. Note that input will pop up a dialog box.
+Enter Sarah in the pop-up box when you are prompted so your output will
+match the desired output.
+'''
+
+name = input("Enter your name")
+print(f"Hello {name}")
