1ed51e107d834d3df1da86c78fa3c1bdc27ea549
MusaAkyuz <musakyuz0@gmail.com>
2022-10-03 13:34:49 +0300

    update

---
+++ 101/dictionaries.py
@@ -0,0 +1,17 @@
+my_dict = {1: "musa", 2: "akyuz", 3: "furkan"}
+
+# get function
+print(my_dict.get(1))
+# output musa
+
+# giving key value
+print(my_dict[2])
+# output akyuz
+
+my_dict[4] = "akyuz"
+
+print(my_dict.values())
+
+print(my_dict.get(9))
+# return none
+
---
+++ 101/strings.py
@@ -0,0 +1,8 @@
+s = "abcdefghjklmn"
+
+print(s[1])  # b
+print(s[1:5])  # bcde
+print(len(s))  # 13
+
+for item in s:
+    print(item, end="")  # abcdefghjklmn
