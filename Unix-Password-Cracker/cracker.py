cd5bf1cebf2bb07231dc9137bd27e4e18f06be88
Musa Akyüz <62879859+MusaAkyuz@users.noreply.github.com>
2022-10-12 18:12:51 +0300

    not completed

--- Unix-Password-Cracker/cracker.py
+++ Unix-Password-Cracker/cracker.py
@@ -3,7 +3,8 @@
 # Musa Akyüz
 
 # The crypt module is not supported on Windows
-# import crypt
+import crypt
+import os
 import re
 
 
@@ -15,8 +16,17 @@ import re
 	Find your unix hashed password on this file;
  	/etc/shadow (needed root previlege)
 '''
-def testPass(password):
-	pass
+def testPass(password, dictFilePath):
+
+	# CHECKING ON DICTIONARY
+	dictFile = open(dictFilePath, "r")
+	for word in dictFile.readlines():
+		word = word.strip("\n")
+		cryptWord = crypt.crypt(word, crypt.METHOD_SHA256)
+		print("word : " + word + "cryptWord : " + cryptWord)
+		if cryptWord == password:
+			return "[+] Found password : " + word + "\n"
+	return "[-] Not in dictionary file !!!"
 
 '''
 	Not complex hash identifier
@@ -36,9 +46,9 @@ def testPass(password):
 def hashIdentifier(password):
 	if password[0] == "$":
 		salt = re.search(r'\$(.*)\$', password).group(1)
-	
-	salt = str(salt)
-
+	else:
+		salt = None
+		
 	if salt == "1":
 		return ["MD5", password[3:]]
 	elif salt == "2a" or salt == "2y":
@@ -50,6 +60,12 @@ def hashIdentifier(password):
 	else:
 		return ["Didnt found", password]
 	
+def main():
+	testPass("$5$EU9QNNm6Tpk3oizd$o6r.l4RtEN8JFKmVmr3EHqsd2ZERkBJVGTWrDekHGg7", "dict.txt")
+			
+if __name__ == "__main__":
+	main()
+
 
 
 	
