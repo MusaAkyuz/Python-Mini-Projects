f9228967b844fea42872ed133a1fc6c67811d7c7
Musa Akyüz <62879859+MusaAkyuz@users.noreply.github.com>
2022-10-13 21:46:12 +0300

    Update islandProblem.py

--- 101/islandProblem.py
+++ 101/islandProblem.py
@@ -3,7 +3,7 @@ grid = [
   ["1","1","1","1","0"],
   ["1","1","0","1","0"],
   ["1","1","0","0","0"],
-  ["0","0","0","0","0"]
+  ["0","0","0","1","0"]
 ]
 
 rowCount = len(grid)
@@ -13,7 +13,7 @@ colCount = len(grid[0])
 # convert this zero to be sea not an island and increase the island number
 # r and c is a starting point
 def convertZero(r, c):
-    grid[i][j] = "0"
+    grid[r][c] = "0"
     for [x, y] in [[r-1, c], [r+1, c], [r, c-1], [r, c+1]]:
         if 0 <= x < rowCount and 0 <= y < colCount and grid[x][y] == "1":
             convertZero(x, y)
