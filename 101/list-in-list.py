bd89ed21726dbee794266415e84c8f8e46b3fdcf
Musa Akyüz <62879859+MusaAkyuz@users.noreply.github.com>
2022-10-13 21:12:58 +0300

    Update list-in-list.py

--- 101/list-in-list.py
+++ 101/list-in-list.py
@@ -17,3 +17,23 @@ print(len(lis[0]))
 #--------
 #3
 #3
+
+# Appending list in list
+list1 = [1, 2, 3]
+list2 = [4, 5 ,6]
+
+list1.extend(list2)
+print(list1)
+
+# OUTPUT
+#--------
+#[1, 2, 3, 4, 5, 6]
+
+list3 = [7, 8, 9]
+list3.append(list2)
+print(list3)
+
+# OUTPUT
+#--------
+#[7, 8, 9, [4, 5, 6]]
+
