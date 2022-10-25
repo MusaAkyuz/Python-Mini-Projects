3ae0b46d518aa45e562717b8cb8f24ff541d04d4
Musa Akyüz <62879859+MusaAkyuz@users.noreply.github.com>
2022-10-26 02:12:41 +0300

    Create 1662.CheckIfTwoStringArraysareEquivalent-----OneLine.py

---
+++ LeetCode/1662.CheckIfTwoStringArraysareEquivalent-----OneLine.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def arrayStringsAreEqual(self, word1, word2):
+        """
+        :type word1: List[str]
+        :type word2: List[str]
+        :rtype: bool
+        """
+
+
+        """
+        Mission
+        Given two string arrays word1 and word2, return true if the two arrays represent the same string, 
+        and false otherwise.
+
+        A string is represented by an array if the array elements concatenated in order forms the string.
+        """
+
+        return "".join(word1) == "".join(word2)
