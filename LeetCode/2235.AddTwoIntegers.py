a58cfe17e3cc6bd6dbf5e7915b6aac1aeb54ccda
Musa Akyüz <62879859+MusaAkyuz@users.noreply.github.com>
2022-10-26 01:55:22 +0300

    Create 2235.AddTwoIntegers.py

---
+++ LeetCode/2235.AddTwoIntegers.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def sum(self, num1, num2):
+        """
+        :type num1: int
+        :type num2: int
+        :rtype: int
+        """
+
+        """
+        Mission
+        Given two integers num1 and num2, return the sum of the two integers.
+        """
+        return num1 + num2
