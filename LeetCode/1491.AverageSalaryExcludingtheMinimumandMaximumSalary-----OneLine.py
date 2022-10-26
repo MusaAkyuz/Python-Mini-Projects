6ad019d03658cb379496b4b7da207644c5e5ce27
Musa Akyüz <62879859+MusaAkyuz@users.noreply.github.com>
2022-10-26 03:29:28 +0300

    Create 1491.AverageSalaryExcludingtheMinimumandMaximumSalary-----OneLine.py

---
+++ LeetCode/1491.AverageSalaryExcludingtheMinimumandMaximumSalary-----OneLine.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def average(self, salary):
+        """
+        :type salary: List[int]
+        :rtype: float
+        """
+
+        """
+        Mission
+        You are given an array of unique integers salary where salary[i] is the salary of the ith employee.
+        Return the average salary of employees excluding the minimum and maximum salary. 
+        Answers within 10-5 of the actual answer will be accepted.
+        """
+
+        return (sum(salary) - min(salary) - max(salary)) / (len(salary) - 2)
