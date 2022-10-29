891dd6d0cc71953a97b2cfab06e345053697c838
Musa Akyüz <62879859+MusaAkyuz@users.noreply.github.com>
2022-10-29 20:37:17 +0300

    Update 1342.NumberofStepstoReduceaNumbertoZero.py

--- LeetCode/1342.NumberofStepstoReduceaNumbertoZero.py
+++ LeetCode/1342.NumberofStepstoReduceaNumbertoZero.py
@@ -1,24 +1,26 @@
-class Solution(object):
-    def numberOfSteps(self, num):
-        """
-        :type num: int
-        :rtype: int
-        """
-
-        """
-        Mission
+public class Solution {
+    public int NumberOfSteps(int num) {
+        
+        */
         Given an integer num, return the number of steps to reduce it to zero.
-
-        In one step, if the current number is even, you have to divide it by 2, otherwise, you have to 
-        subtract 1 from it.
-        """
-
-        count = 0
-        while num > 0:
-            if num%2 == 0:
-                num = num/2
-                count += 1
-            else:
-                num = num-1
-                count += 1
-        return count
+        In one step, if the current number is even, you have to divide it by 2, otherwise, 
+        you have to subtract 1 from it.
+        /*
+        
+        int count = 0;
+        while(num > 0)
+        {
+            if(num%2 == 0)
+            {
+                num = num/2;
+                count++;
+            }
+            else
+            {
+                num = num-1;
+                count++;
+            }
+        }
+            return count;
+    }
+}
