31ba22921d6440656f06f0e7e100ccf8cbd7e59b
Musa Akyüz <62879859+MusaAkyuz@users.noreply.github.com>
2022-11-05 23:17:23 +0300

    Create 1920.BuildArrayfromPermutation.py

---
+++ LeetCode/1920.BuildArrayfromPermutation.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def buildArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+
+        """
+        Mission
+        Given a zero-based permutation nums (0-indexed), build an array ans of the same 
+        length where ans[i] = nums[nums[i]] for each 0 <= i < nums.length and return it.
+
+        A zero-based permutation nums is an array of distinct integers from 0 to nums.length - 1 
+        (inclusive).
+        """
+
+
+        res = []
+        for item in nums:
+            res.append(nums[item])
+        return res
