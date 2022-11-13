544a9eb0c8a136c56dfb8b8ca390e57ba4d9adc4
Musa Akyüz <62879859+MusaAkyuz@users.noreply.github.com>
2022-11-13 05:21:21 +0300

    Create 151.ReverseWordsinaString-----OneLine.py

---
+++ LeetCode/151.ReverseWordsinaString-----OneLine.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def reverseWords(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+
+        """
+        Mission
+        Given an input string s, reverse the order of the words.
+
+        A word is defined as a sequence of non-space characters. 
+        The words in s will be separated by at least one space.
+
+        Return a string of the words in reverse order concatenated by a single space.
+
+        Note that s may contain leading or trailing spaces or multiple spaces 
+        between two words. The returned string should only have a single space 
+        separating the words. Do not include any extra spaces.
+        """
+
+        return " ".join(s.split()[::-1])
+        
