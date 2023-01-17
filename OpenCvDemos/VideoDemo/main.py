8f1a8e0c7b918092a6f0886f2c8eaf26068f933c
MusaAkyuz <musakyuz0@gmail.com>
2023-01-17 09:21:31 +0300

    opencv and icmplib demos update

--- OpenCvDemos/VideoDemo/main.py
+++ OpenCvDemos/VideoDemo/main.py
@@ -23,7 +23,7 @@ def region(image):
     height, width = image.shape
 
     triangle = np.array([
-        [(100, height), (475, 325), (width, height)]
+        [(100, height), (800, 10), (width, height)]
     ])
     # siyah
     mask = np.zeros_like(image)
@@ -47,11 +47,14 @@ def display_lines(image, lines):
 def average(image, lines):
     left = []
     right = []
+
     if lines is not None:
         for line in lines:
+            print(line)
             x1, y1, x2, y2 = line.reshape(4)
 
             parameters = np.polyfit((x1, x2), (y1, y2), 1)
+            print(parameters)
             slope = parameters[0]
             y_int = parameters[1]
 
@@ -61,8 +64,8 @@ def average(image, lines):
                 right.append((slope, y_int))
 
     # column average
-    right_avg = np.average(np.array(right), axis=0)
-    left_avg = np.average(np.array(left), axis=0)
+    right_avg = np.average(right, axis=0)
+    left_avg = np.average(left, axis=0)
     # lines based on average
     left_line = make_points(image, left_avg)
     right_line = make_points(image, right_avg)
@@ -74,12 +77,13 @@ def make_points(image, average):
     slope, y_int = average
     y1 = image.shape[0]
     y2 = int(y1 * (3 / 5))
-    # biraz da math
+    # bbiraz da math
     x1 = int((y1 - y_int) // slope)
     x2 = int((y2 - y_int) // slope)
     return np.array([x1, y1, x2, y2])
 
 
+'''
 cap = cv2.VideoCapture('road.mp4')
 
 while cap.isOpened():
@@ -90,26 +94,70 @@ while cap.isOpened():
         break
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    #cv2.imshow('frame', gray)
+    cv2.imshow('frame', gray)
 
     copy = np.copy(gray)
-    edges = cv2.Canny(copy, 10, 125, 1)
+    edges = cv2.Canny(copy, 50, 150)
     isolated = region(edges)
     cv2.imshow("edges", edges)
     cv2.imshow("isolated", isolated)
-    cv2.waitKey(0)
 
     lines = cv2.HoughLinesP(isolated, 2, np.pi / 180, 100, np.array([]), minLineLength=40, maxLineGap=5)
+
     averaged_lines = average(copy, lines)
-    cv2.imshow("avar", averaged_lines)
     black_lines = display_lines(copy, averaged_lines)
 
     lanes = cv2.addWeighted(copy, 0.8, black_lines, 1, 1)
     cv2.imshow("lanes", lanes)
-    cv2.waitKey(0)
 
     if cv2.waitKey(10) == ord('q'):
         break
+
+cap.release()
+cv2.destroyAllWindows()
+'''
+
+'''
+image_path = r"lane.jpg"
+image1 = cv2.imread(image_path)
+plt.imshow(image1)
+'''
+
+cap = cv2.VideoCapture('road2.mp4')
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    # if frame is read correctly ret is True
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+
+    # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # cv2.imshow('frame', frame)
+    try:
+        copy = np.copy(frame)
+        edges = cv2.Canny(copy, 50, 150)
+        isolated = region(edges)
+        # cv2.imshow("edges",edges)
+        # cv2.imshow("isolated",isolated)
+
+        lines = cv2.HoughLinesP(isolated, 2, np.pi / 180, 100, np.array([]), minLineLength=40, maxLineGap=5)
+        averaged_lines = average(copy, lines)
+        black_lines = display_lines(copy, averaged_lines)
+
+        lanes = cv2.addWeighted(copy, 0.8, black_lines, 1, 1)
+        cv2.imshow("lanes", lanes)
+
+        '''
+        if cv2.waitKey(0) == ord('q'):
+            break
+        '''
+
+    except:
+        pass
+
+    finally:
+        cv2.waitKey(100)
 
 cap.release()
 cv2.destroyAllWindows()
